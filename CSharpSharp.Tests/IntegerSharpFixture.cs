643d65e1488b44d28c16e7c15a136c34212d60c8
mbillard <mbillard@localhost>
2011-04-12 23:17:06 -0400

    Added an iterating method on integer and long values that can receive a step as parameter.

---
+++ CSharpSharp.Tests/IntegerSharpFixture.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using MbUnit.Framework;
+
+namespace CSharpSharp.Tests
+{
+    /// <summary>
+    /// Validates the IntegerSharp properties, methods and extension methods.
+    /// </summary>
+    public class IntegerSharpFixture
+    {
+        /// <summary>
+        /// Validates that the integer iterating method behaves as expected.
+        /// </summary>
+        [Test]
+        [Row(1, 5, null, 5)]
+        [Row(1, 5, 2, 3)]
+        [Row(1, 5, 3, 2)]
+        public void IntegerStepping(int start, int end, int? step, int expectedNumberOfIterations)
+        {
+            int numberOfIterations = 0;
+
+            if (step.HasValue)
+            {
+                foreach (int integer in start.To(end, step.Value))
+                {
+                    numberOfIterations++;
+                }
+            }
+            else
+            {
+                foreach (int integer in start.To(end))
+                {
+                    numberOfIterations++;
+                }
+            }
+
+            Assert.AreEqual(expectedNumberOfIterations, numberOfIterations);
+        }
+
+        /// <summary>
+        /// Validates that the integer iterating method behaves as expected.
+        /// </summary>
+        [Test]
+        [Row(1, 5, null, 5)]
+        [Row(1, 5, 2, 3)]
+        [Row(1, 5, 3, 2)]
+        public void IntegerStepping(long start, long end, int? step, long expectedNumberOfIterations)
+        {
+            long numberOfIterations = 0;
+
+            if (step.HasValue)
+            {
+                foreach (long integer in start.To(end, step.Value))
+                {
+                    numberOfIterations++;
+                }
+            }
+            else
+            {
+                foreach (long integer in start.To(end))
+                {
+                    numberOfIterations++;
+                }
+            }
+
+            Assert.AreEqual(expectedNumberOfIterations, numberOfIterations);
+        }
+    }
+}
---
+++ CSharpSharp/IntegerSharp.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpSharp
+{
+    /// <summary>
+    /// Provides additional methods for working with integer (or long) values.
+    /// </summary>
+    public static class IntegerSharp
+    {
+        /// <summary>
+        /// Iterates between integers from start to end and
+        /// incrementing by the step at each iteration.
+        /// </summary>
+        /// <param name="start">The start.</param>
+        /// <param name="end">The end.</param>
+        /// <param name="step">The step.</param>
+        /// <returns>An enumeration of all the steps from start to end.</returns>
+        public static IEnumerable<int> To(this int start, int end, int step = 1)
+        {
+            int currentIteration = start;
+
+            while (currentIteration <= end)
+            {
+                yield return currentIteration;
+
+                currentIteration += step;
+            }
+        }
+
+        /// <summary>
+        /// Iterates between longs from start to end and
+        /// incrementing by the step at each iteration.
+        /// </summary>
+        /// <param name="start">The start.</param>
+        /// <param name="end">The end.</param>
+        /// <param name="step">The step.</param>
+        /// <returns>An enumeration of all the steps from start to end.</returns>
+        public static IEnumerable<long> To(this long start, long end, long step = 1)
+        {
+            long currentIteration = start;
+
+            while (currentIteration <= end)
+            {
+                yield return currentIteration;
+
+                currentIteration += step;
+            }
+        }
+    }
+}
